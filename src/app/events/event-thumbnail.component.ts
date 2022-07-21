2ae10c98a920a5db5a16222ff58b75f45948454d
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2022-07-21 16:14:48 +0545

    applying inline styles to component

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -10,12 +10,16 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     <div>Price : \${{event.price}}</div>
     <div>
       <span>Location: {{event.location.address}}</span>
-      <span>&nbsp;</span>
-      <span>{{event.location.city}}, {{event.location.country}}</span>
+      <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
     </div>
     <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
   </div>
-  `
+  `,
+  styles: [`
+    .pad-left{ margin-left: 10px}
+    .well div{ color : red}
+  `]
+
 })
 export class EventThumbnailComponent{
 
