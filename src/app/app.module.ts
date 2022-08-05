d5f5c9ff79f57fc75a1e798d0da2613dfdde3704
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2022-08-05 18:50:32 +0545

    implemented Can active route guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
+import { Error404Component } from './errors/404.component';
 
 import { EventsAppComponent } from './events-app.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventDetailsComponent } from './events/events-details/event-details.component';
+import { EventRouteActivator } from './events/events-details/event-route-activator.service';
 import { EventListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
@@ -20,12 +22,13 @@ import { appRoutes } from './routes';
     EventDetailsComponent,
     NavBarComponent,
     CreateEventComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [EventService,ToastrService],
+  providers: [EventService,ToastrService,EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from "@angular/router";
+import { Error404Component } from "./errors/404.component";
 import { CreateEventComponent } from "./events/create-event.component";
 import { EventDetailsComponent } from "./events/events-details/event-details.component";
+import { EventRouteActivator } from "./events/events-details/event-route-activator.service";
 import { EventListComponent } from "./events/events-list.component";
 
 export const appRoutes:Routes = [
   { path:'events/new', component: CreateEventComponent},
   { path: 'events', component: EventListComponent },
-  { path: 'events/:id', component: EventDetailsComponent },
+  { path: 'events/:id', component: EventDetailsComponent,
+   canActivate:[EventRouteActivator] },
+  {path: '404', component:Error404Component},
   { path: '', redirectTo:'/events', pathMatch:'full'}
 ]
