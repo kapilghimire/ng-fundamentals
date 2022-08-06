8f8ecc0e681ba74b6d424e5d132ee3bd5a7413de
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2022-08-06 08:02:11 +0545

    used barrel to clean the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+
+import {
+  CreateEventComponent,
+  EventListResolver,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  EventRouteActivator,
+  EventListComponent,
+  EventService
+} from './events/index'
+
 import { ToastrService } from './common/toastr.service';
 import { Error404Component } from './errors/404.component';
 
 import { EventsAppComponent } from './events-app.component';
-import { CreateEventComponent } from './events/create-event.component';
-import { EventListResolver } from './events/event-list-resolver.service';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventDetailsComponent } from './events/events-details/event-details.component';
-import { EventRouteActivator } from './events/events-details/event-route-activator.service';
-import { EventListComponent } from './events/events-list.component';
-import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
 
