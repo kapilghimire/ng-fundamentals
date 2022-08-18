9eb7cab9bf6c93fa0e9e739f1925511b6ffa4290
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2022-08-18 11:46:38 +0545

    implemented reactive forms

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -3,17 +3,27 @@ import { IUser } from "./user.model";
 
 @Injectable()
 export class AuthService{
+
   currentUser: IUser | undefined
+
   loignUser(userName:string,password:string){
     this.currentUser = {
       id:1,
-      userName:'Kapil',
       lastName:'Ghimire',
-      firstName:'Kapil'
+      firstName:'Kapil',
+      userName : userName,
     }
   }
 
   isAuthenticate(){
     return !!this.currentUser;
+  }
+
+  updateCurretnUser(firstName: string, lastName: string) {
+    if(this.currentUser){
+      this.currentUser.firstName=firstName;
+      this.currentUser.lastName=lastName;
+    }
+
   }
 }
--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup = new FormGroup({}) ;
 
+  constructor(private authService: AuthService,private router:Router){}
+
+  ngOnInit(): void {
+    let firstName =new FormControl(this.authService.currentUser?.firstName);
+    let lastName = new FormControl(this.authService.currentUser?.lastName);
+    this.profileForm=  new FormGroup({
+      firstName:firstName,
+      lastName: lastName
+    })
+  }
+
+  cancel(){
+    this.router.navigate(['events']);
+  }
+
+  saveProfile(formVales:any){
+     this.authService.updateCurretnUser(formVales.firstName,formVales.lastName)
+     this.router.navigate(['events'])
+  }
 }
