a8779951160312465e93b8195a571322b1633fa5
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2022-08-31 15:21:33 +0530

    implemented filtering of data

--- src/app/events/events-details/session-list.component.ts
+++ src/app/events/events-details/session-list.component.ts
@@ -1,12 +1,33 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { ISession } from "../shared";
 
 @Component({
   templateUrl:`./session-list.component.html`,
   selector:'session-list'
 })
-export class SessionListComponent{
+export class SessionListComponent implements OnChanges{
 
-  @Input() sessions:ISession[] | undefined =[];
 
+  @Input() sessions:ISession[] | undefined=[];
+  @Input() filterBy!:string
+  visibleSessions: ISession[] = [];
+
+  /*  what is this method with SimpleChanges
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error("Method not implemented.");
+  }
+   */
+
+  ngOnChanges(): void {
+    if(this.sessions){
+      this.filterSessions(this.filterBy);
+    }
+  }
+  filterSessions(filterBy: string) {
+    if(filterBy == 'all'){
+       this.visibleSessions = this.sessions!.slice(0);
+    }else{
+      this.visibleSessions = this.sessions!.filter(s=>s.level.toLocaleLowerCase()===filterBy);
+    }
+  }
 }
